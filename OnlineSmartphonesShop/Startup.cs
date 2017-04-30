5d4d2be90fd9cabc6b9f86b16514a30032b8c4a2
Алекс Инджов <sashe94@hotmail.com>
2017-05-01 00:34:04 +0300

    Init Commit This is the startingpoint of the project and it will be updated frecuently

---
+++ OnlineSmartphonesShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnlineSmartphonesShop.Startup))]
+namespace OnlineSmartphonesShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
